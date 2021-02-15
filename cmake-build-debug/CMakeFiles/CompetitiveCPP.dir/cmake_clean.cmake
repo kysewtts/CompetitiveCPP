9bdff787daff80cf7c4f712cf3cd57181c826297
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2021-02-15 09:45:55 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CompetitiveCPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompetitiveCPP.dir/main.cpp.o"
+  "CompetitiveCPP"
+  "CompetitiveCPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompetitiveCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

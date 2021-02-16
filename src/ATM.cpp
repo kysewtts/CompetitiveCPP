3d0974757b911f2ffe7d988973cee831dc028e9e
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2021-02-16 09:46:30 +0530

    HS08TEST

---
+++ src/ATM.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Ujjwal Prakash on 15/02/21.
+//
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int withdraw;
+    float balance;
+    cin >> withdraw >> balance;
+    cout << fixed;
+    if (withdraw % 5 == 0 && balance >= (withdraw + 0.5)) {
+        cout << setprecision(2) << balance - withdraw - 0.5 << "\n";
+    } else {
+        cout << setprecision(2) << balance << "\n";
+    }
+    return 0;
+}
